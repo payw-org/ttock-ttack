f8d063ef8f0518b321d3b485925d0e00cfb4da74
choibumsu <bumsu0211@gmail.com>
2021-01-10 02:14:23 +0900

    [#2] feat : Timer 컴포넌트 작성
    - Date 객체를 props로 받아서, D-day까지 남은 시간을 표시하는 컴포넌트
    - 남은 시간을 계산하여, 일, 시간, 분, 초에 맞는 DisplayTime 컴포넌트를 호출

---
+++ src/components/Timer/index.tsx
@@ -0,0 +1,58 @@
+import { getDDay } from '@/utils/time'
+import React, { useState } from 'react'
+import DisplayTime from './DisplayTime'
+import './style.scss'
+
+interface TimerProps {
+  dateTime: Date
+}
+
+function getTwoDigit(num: number): string {
+  return num < 10 ? `0${num}` : num.toString()
+}
+
+const Timer: React.FC<TimerProps> = ({ dateTime }) => {
+  const [currentDateTime, setCurrentDateTime] = useState(new Date())
+  setTimeout(() => {
+    setCurrentDateTime(new Date())
+  }, 1000)
+
+  const timeGap: Date = new Date(dateTime.getTime() - currentDateTime.getTime())
+  const [restDay, hour, minute, second] = getDDay(timeGap).map((v) =>
+    getTwoDigit(v)
+  )
+
+  const nextTimeGap: Date = new Date(
+    dateTime.getTime() - currentDateTime.getTime() - 1000
+  )
+  const [nextRestDay, nextHour, nextMinute, nextSecond] = getDDay(
+    nextTimeGap
+  ).map((v) => getTwoDigit(v))
+
+  return (
+    <div className="timer">
+      <DisplayTime
+        currentTime={restDay}
+        nextTime={nextRestDay}
+        unit="일"
+      ></DisplayTime>
+      <DisplayTime
+        currentTime={hour}
+        nextTime={nextHour}
+        unit="시간"
+      ></DisplayTime>
+      <DisplayTime
+        currentTime={minute}
+        nextTime={nextMinute}
+        unit="분"
+      ></DisplayTime>
+      <DisplayTime
+        currentTime={second}
+        nextTime={nextSecond}
+        unit="초"
+      ></DisplayTime>
+    </div>
+  )
+}
+
+export default Timer
