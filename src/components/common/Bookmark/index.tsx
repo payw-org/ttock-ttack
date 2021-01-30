ab1ea6b67df9ef36aa937b717273860c26d59156
choibumsu <bumsu0211@gmail.com>
2021-01-31 02:38:40 +0900

    [#8] feat : Bookmark 컴포넌트 작성 및 스타일 작업

---
+++ src/components/common/Bookmark/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './style.scss'
+
+export interface BookmarkProps {
+  name: string
+  url: string
+  image: string
+}
+
+export const Bookmark: React.FC<BookmarkProps> = ({ name, url, image }) => {
+  return (
+    <div className="bookmark">
+      <a href={url} target="_blank">
+        <img loading="lazy" src={image} alt={name} />
+        <div className="name">{name}</div>
+      </a>
+    </div>
+  )
+}
