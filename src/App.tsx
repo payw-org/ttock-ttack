62bbb13e117cec9a3e61a79a76a8a9adb182d913
choibumsu <bumsu0211@gmail.com>
2021-02-07 18:28:52 +0900

    [#11] feat - axios 활용하여 data.json 호출

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Timer } from '@/components/Timer'
 import { SideMenuBar } from '@/components/SideMenuBar'
 import { testBookmarkList, testDateTime } from './utils/mock-data'
 import '@/style/fonts.scss'
 import '@/style/global.scss'
+import axios from 'axios'
 
-function App() {
+type Data = {
+  sideBookmarkList: Array<any>
+  mainDate: string
+}
+
+const getData = async () => {
+  const response = await axios.get('/data/data.json')
+  return response.data
+}
+
+const App: React.FC = () => {
+  const [data, setData] = useState<Data>()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedData = await getData()
+      setData(fetchedData)
+    }
+    fetchData()
+  }, [])
+
   return (
     <div>
-      <Timer dateTime={testDateTime} />
-      <SideMenuBar bookmarkList={testBookmarkList} />
+      {data ? (
+        <>
+          <Timer dateTime={new Date(data.mainDate)} />
+          <SideMenuBar bookmarkList={data.sideBookmarkList} />
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   )
 }
