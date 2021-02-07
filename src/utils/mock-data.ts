6b9dbba5cb5890d637430fb47af7ce7fa2436a31
choibumsu <bumsu0211@gmail.com>
2021-02-07 17:28:04 +0900

    [#11] fix : scss, ts 파일 prettier로 formatting

--- src/utils/mock-data.ts
+++ src/utils/mock-data.ts
@@ -2,42 +2,42 @@ export const testBookmarkList = [
   {
     name: '중앙대학교',
     url: 'https://www.cau.ac.kr/index.do',
-    image: '/images/cau-logo.png'
+    image: '/images/cau-logo.png',
   },
   {
     name: '중앙대학교',
     url: 'https://www.cau.ac.kr/index.do',
-    image: '/images/cau-logo.png'
+    image: '/images/cau-logo.png',
   },
   {
     name: '중앙대학교',
     url: 'https://www.cau.ac.kr/index.do',
-    image: '/images/cau-logo.png'
+    image: '/images/cau-logo.png',
   },
   {
     name: '중앙대학교',
     url: 'https://www.cau.ac.kr/index.do',
-    image: '/images/cau-logo.png'
+    image: '/images/cau-logo.png',
   },
   {
     name: '중앙대학교',
     url: 'https://www.cau.ac.kr/index.do',
-    image: '/images/cau-logo.png'
+    image: '/images/cau-logo.png',
   },
   {
     name: '중앙대학교',
     url: 'https://www.cau.ac.kr/index.do',
-    image: '/images/cau-logo.png'
+    image: '/images/cau-logo.png',
   },
   {
     name: '중앙대학교',
     url: 'https://www.cau.ac.kr/index.do',
-    image: '/images/cau-logo.png'
+    image: '/images/cau-logo.png',
   },
   {
     name: '중앙대학교',
     url: 'https://www.cau.ac.kr/index.do',
-    image: '/images/cau-logo.png'
+    image: '/images/cau-logo.png',
   },
 ]
 
