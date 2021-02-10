36770e1ffa36119c885a1eda8431444bf20ef30c
choibumsu <bumsu0211@gmail.com>
2021-02-10 23:04:03 +0900

    [#11] feat : 포맷에 따라 날짜 반환하는 함수 작성

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -7,3 +7,38 @@ export function getDDay(dateTime: Date): number[] {
   ]
   return [restDay, hour, minute, second]
 }
+
+export function getFormattedDate(date: Date, format: string): string {
+  const [year, month, day, hour, minute, second] = [
+    date.getFullYear(),
+    date.getMonth() + 1,
+    date.getDate(),
+    date.getUTCHours(),
+    date.getUTCMinutes(),
+    date.getUTCSeconds(),
+  ]
+  const DATE_FORMAT = {
+    YYYY: year,
+    YY: year % 100,
+    MM: month < 10 ? `0${month}` : month,
+    M: month,
+    DD: day < 10 ? `0${day}` : day,
+    D: day,
+    hh: hour < 10 ? `0${hour}` : hour,
+    h: hour,
+    mm: minute < 10 ? '0${minute}' : minute,
+    m: minute,
+    ss: second < 10 ? `0${second}` : second,
+    s: second,
+  }
+
+  let formattedDate = format
+  for (let key in DATE_FORMAT) {
+    formattedDate = formattedDate.replace(
+      new RegExp(key, 'g'),
+      DATE_FORMAT[key]
+    )
+  }
+
+  return formattedDate
+}
