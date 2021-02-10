dc3a2637c5a0d82ce6085e43d061665db027d144
choibumsu <bumsu0211@gmail.com>
2021-02-10 23:32:35 +0900

    [#11] feat : TodoContainer에 토글버튼 추가

--- src/components/TodoContainer/index.tsx
+++ src/components/TodoContainer/index.tsx
@@ -12,16 +12,23 @@ export const TodoContainer: React.FC<TodoContainerProps> = ({
   todoSectionList,
 }) => {
   const [isShowTodo, setIsShowTodo] = useState(isShow)
+  const toggleContainer = () => {
+    setIsShowTodo(!isShowTodo)
+  }
 
   return (
-    <div className="todo-container" data-component="">
-      <div
-        className="row between title-wrapper"
-        onClick={() => {
-          setIsShowTodo(!isShowTodo)
-        }}
-      >
+    <div
+      className={'todo-container ' + (isShowTodo ? 'minus' : 'plus')}
+      data-component=""
+    >
+      <div className="row between title-wrapper">
         <div className="title">To Do List</div>
+        <i className="f7-icons plus" onClick={toggleContainer}>
+          plus_square
+        </i>
+        <i className="f7-icons minus" onClick={toggleContainer}>
+          minus_square
+        </i>
       </div>
       <div className={'section-container ' + (!isShowTodo ? 'dp-none' : '')}>
         {todoSectionList.map((todoSection) => (
