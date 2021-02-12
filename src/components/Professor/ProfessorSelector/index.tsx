783e851ae7bef03293c29fed2db3396f71c6116d
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-02-13 03:59:20 +0900

    [#3] feat : Professor Selector 추가

---
+++ src/components/Professor/ProfessorSelector/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './style.scss'
+
+export interface SelectorBarProps {
+  selector:string[]
+  filterSelector: (value: string) => void
+}
+
+export const SelectorBar: React.FC<SelectorBarProps> = ({ selector, filterSelector }) => {
+  const onChangeSelect = (e) => {
+    filterSelector(e.target.value);
+  };
+
+  return (
+    <div className="custom-select">
+      <select onChange={onChangeSelect}>
+        {selector.map((name, index) => (
+          <option key={index} value={name}>{name}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
