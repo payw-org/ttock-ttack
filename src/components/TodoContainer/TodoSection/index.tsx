faf581cbe5af01952c5fbfca7de5a0d0bcb4e92c
choibumsu <bumsu0211@gmail.com>
2021-02-18 01:57:58 +0900

    [#15] refact : Todo 정렬 순서 조정
    새로운 Todo는 모두 위, 기존 Todo는 편집하더라도 기존 자리 유지

--- src/components/TodoContainer/TodoSection/index.tsx
+++ src/components/TodoContainer/TodoSection/index.tsx
@@ -28,10 +28,11 @@ export const TodoSection: React.FC<TodoSectionProps> = (props) => {
   }
 
   const addTodo = () => {
-    const newTodo = {
+    const newTodo: TodoProps = {
       ...defaultTodo,
       id: todoList.length + 1,
       isEdit: true,
+      isNew: true,
     }
     dispatchStore('todoList', [newTodo, ...todoList])
 
@@ -64,7 +65,7 @@ export const TodoSection: React.FC<TodoSectionProps> = (props) => {
       </div>
       <div className={'todo-container ' + (!isShow ? 'dp-none' : '')}>
         {todoList
-          .filter((todo) => todo.sectionId === props.id && todo.isEdit)
+          .filter((todo) => todo.sectionId === props.id && todo.isNew)
           .map((todo) => (
             <TodoEdit
               {...todo}
@@ -73,10 +74,18 @@ export const TodoSection: React.FC<TodoSectionProps> = (props) => {
             ></TodoEdit>
           ))}
         {todoList
-          .filter((todo) => todo.sectionId === props.id && !todo.isEdit)
-          .map((todo) => (
-            <Todo {...todo} key={todo.id + todo.title}></Todo>
-          ))}
+          .filter((todo) => todo.sectionId === props.id && !todo.isNew)
+          .map((todo) =>
+            todo.isEdit ? (
+              <TodoEdit
+                {...todo}
+                sectionId={props.id}
+                key={todo.id + todo.title}
+              ></TodoEdit>
+            ) : (
+              <Todo {...todo} key={todo.id + todo.title}></Todo>
+            )
+          )}
       </div>
     </div>
   )
