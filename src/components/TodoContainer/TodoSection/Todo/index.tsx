fa693018d899a5b702f36875db1540dcb7535e63
choibumsu <bumsu0211@gmail.com>
2021-02-18 01:59:20 +0900

    [#15] feat : Todo 수정, 삭제 기능 추가
    menu 추가
    수정, 삭제 기능 추가

--- src/components/TodoContainer/TodoSection/Todo/index.tsx
+++ src/components/TodoContainer/TodoSection/Todo/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useStore } from '@/store'
 import { getFormattedDate } from '@/utils/time'
 import './style.scss'
 
@@ -12,16 +13,78 @@ export interface TodoProps {
   isNew?: boolean
 }
 
-export const Todo: React.FC<TodoProps> = ({ title, dueDate, isMain }) => {
+export const Todo: React.FC<TodoProps> = ({ id, title, dueDate, isMain }) => {
+  const {
+    store: { todoList },
+    dispatchStore,
+  } = useStore()
+  const thisTodo = todoList.find((todo) => todo.id === id)
+
+  const [isShowMenu, setIsShowMenu] = useState(false)
+  useEffect(() => {
+    return () => setIsShowMenu(false)
+  }, [])
+
+  const closeMenu = (e) => {
+    const todoComponent = e.target.closest('.todo')
+    if (todoComponent && +todoComponent.dataset.component === id) {
+      return
+    }
+
+    setIsShowMenu(false)
+    window.removeEventListener('click', closeMenu)
+  }
+
+  const toggleMenu = (e) => {
+    setIsShowMenu(!isShowMenu)
+    window.addEventListener('click', closeMenu)
+  }
+
+  const editTodo = () => {
+    if (!thisTodo) return
+
+    thisTodo.isEdit = true
+    dispatchStore('todoList', [...todoList])
+  }
+
+  const deleteTodo = () => {
+    if (!thisTodo) return
+
+    dispatchStore(
+      'todoList',
+      todoList.filter((todo) => todo.id !== id)
+    )
+  }
+
   return (
-    <div className="todo" data-component="">
+    <div className="todo" data-component={id}>
       <div className="row between">
         <div className="title-wrapper">
           <h3 className="title">{title}</h3>
           <div className={'star' + (isMain ? 'main' : '')}></div>
         </div>
         <div className="menu-wrapper">
-          <i className="f7-icons menu">ellipsis_vertical</i>
+          <i
+            className="f7-icons menu-btn"
+            onClick={(e) => {
+              toggleMenu(e)
+            }}
+          >
+            ellipsis_vertical
+          </i>
+          {isShowMenu ? (
+            <div className="menu-wrapper">
+              <button className="menu">완료</button>
+              <button className="menu" onClick={editTodo}>
+                수정
+              </button>
+              <button className="menu" onClick={deleteTodo}>
+                삭제
+              </button>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
       <div className="row">
