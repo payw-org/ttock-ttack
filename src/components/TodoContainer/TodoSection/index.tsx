4024fc2bfe79368588ba6bda2bd1a87461f028a3
choibumsu <bumsu0211@gmail.com>
2021-02-17 23:58:48 +0900

    [#15] feat : TodoSection Add 기능 추가
    add 버튼 클릭 : defaultTodo 를 store에 추가
    isEdit 상태인 Todo를 앞쪽에 배치

--- src/components/TodoContainer/TodoSection/index.tsx
+++ src/components/TodoContainer/TodoSection/index.tsx
@@ -1,44 +1,82 @@
 import React, { useState } from 'react'
-import './style.scss'
+import { useStore } from '@/store'
 import { Todo, TodoProps } from './Todo'
+import { TodoEdit } from './TodoEdit'
+import './style.scss'
 
 export type TodoSectionProps = {
   id: number
   isShow: boolean
+  isAddable: boolean
   title: string
-  todoList: TodoProps[]
 }
 
-export const TodoSection: React.FC<TodoSectionProps> = ({
-  id,
-  isShow,
-  title,
-  todoList,
-}) => {
-  const [isShowTodo, setIsShowTodo] = useState(isShow)
+export const TodoSection: React.FC<TodoSectionProps> = (props) => {
+  const {
+    store: { todoList },
+    dispatchStore,
+  } = useStore()
+
+  const [isShow, setIsShow] = useState(props.isShow)
+  const defaultTodo: TodoProps = {
+    sectionId: props.id,
+    id: 0,
+    title: '',
+    dueDate: new Date(),
+    isMain: false,
+    isEdit: false,
+  }
+
+  const addTodo = () => {
+    const newTodo = {
+      ...defaultTodo,
+      id: todoList.length + 1,
+      isEdit: true,
+    }
+    dispatchStore('todoList', [newTodo, ...todoList])
+
+    if (!isShow) setIsShow(true)
+  }
 
   return (
     <div className="todo-section">
       <div className="head row between">
         <div className="row title-wrapper">
-          <div className="title">{title}</div>
-          <div className="add-btn">Add</div>
+          <div className="title">{props.title}</div>
+          {props.isAddable ? (
+            <div className="add-btn" onClick={addTodo}>
+              Add
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
         <div className="toggle">
           <i
-            className={'f7-icons ' + (isShowTodo ? 'up' : 'down')}
+            className={'f7-icons ' + (isShow ? 'up' : 'down')}
             onClick={() => {
-              setIsShowTodo(!isShowTodo)
+              setIsShow(!isShow)
             }}
           >
             chevron_up
           </i>
         </div>
       </div>
-      <div className={'todo-container ' + (!isShowTodo ? 'dp-none' : '')}>
-        {todoList.map((todo) => (
-          <Todo {...todo} key={todo.id}></Todo>
-        ))}
+      <div className={'todo-container ' + (!isShow ? 'dp-none' : '')}>
+        {todoList
+          .filter((todo) => todo.sectionId === props.id && todo.isEdit)
+          .map((todo) => (
+            <TodoEdit
+              {...todo}
+              sectionId={props.id}
+              key={todo.id + todo.title}
+            ></TodoEdit>
+          ))}
+        {todoList
+          .filter((todo) => todo.sectionId === props.id && !todo.isEdit)
+          .map((todo) => (
+            <Todo {...todo} key={todo.id + todo.title}></Todo>
+          ))}
       </div>
     </div>
   )
