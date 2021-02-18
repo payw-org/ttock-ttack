99de84d9cefaf023af1f605c58ff4faa95ab600d
choibumsu <bumsu0211@gmail.com>
2021-02-19 02:06:56 +0900

    [#19] feat : TodoSection toggle시 height transition 적용

--- src/components/TodoContainer/TodoSection/index.tsx
+++ src/components/TodoContainer/TodoSection/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useStore } from '@/store'
+import { getFirstChildHeight } from '@/utils/common'
 import { Todo, TodoProps } from './Todo'
 import { TodoEdit } from './TodoEdit'
 import './style.scss'
@@ -13,11 +14,35 @@ export type TodoSectionProps = {
 
 export const TodoSection: React.FC<TodoSectionProps> = (props) => {
   const {
-    store: { todoList },
+    store: { todoSectionList, todoList },
     dispatchStore,
   } = useStore()
-
+  const container = useRef<HTMLDivElement>(null)
   const [isShow, setIsShow] = useState(props.isShow)
+
+  const setContainerHeight = () => {
+    if (!container.current) return
+
+    if (isShow) {
+      container.current.style.height = `${getFirstChildHeight(
+        container.current
+      )}px`
+      container.current.style.opacity = '1'
+    } else {
+      container.current.style.height = '0'
+      container.current.style.opacity = '0'
+    }
+  }
+
+  useEffect(() => {
+    setContainerHeight()
+  }, [isShow, todoList])
+
+  const toggleSection = () => {
+    setIsShow(!isShow)
+    dispatchStore('todoSectionList', [...todoSectionList])
+  }
+
   const defaultTodo: TodoProps = {
     sectionId: props.id,
     id: 0,
@@ -40,7 +65,7 @@ export const TodoSection: React.FC<TodoSectionProps> = (props) => {
   }
 
   return (
-    <div className="todo-section">
+    <div className="todo-section" data-component="">
       <div className="head row between">
         <div className="row title-wrapper">
           <div className="title">{props.title}</div>
@@ -55,37 +80,37 @@ export const TodoSection: React.FC<TodoSectionProps> = (props) => {
         <div className="toggle">
           <i
             className={'f7-icons ' + (isShow ? 'up' : 'down')}
-            onClick={() => {
-              setIsShow(!isShow)
-            }}
+            onClick={toggleSection}
           >
             chevron_up
           </i>
         </div>
       </div>
-      <div className={'todo-container ' + (!isShow ? 'dp-none' : '')}>
-        {todoList
-          .filter((todo) => todo.sectionId === props.id && todo.isNew)
-          .map((todo) => (
-            <TodoEdit
-              {...todo}
-              sectionId={props.id}
-              key={todo.id + todo.title}
-            ></TodoEdit>
-          ))}
-        {todoList
-          .filter((todo) => todo.sectionId === props.id && !todo.isNew)
-          .map((todo) =>
-            todo.isEdit ? (
+      <div className="todo-container" ref={container}>
+        <div>
+          {todoList
+            .filter((todo) => todo.sectionId === props.id && todo.isNew)
+            .map((todo) => (
               <TodoEdit
                 {...todo}
                 sectionId={props.id}
                 key={todo.id + todo.title}
               ></TodoEdit>
-            ) : (
-              <Todo {...todo} key={todo.id + todo.title}></Todo>
-            )
-          )}
+            ))}
+          {todoList
+            .filter((todo) => todo.sectionId === props.id && !todo.isNew)
+            .map((todo) =>
+              todo.isEdit ? (
+                <TodoEdit
+                  {...todo}
+                  sectionId={props.id}
+                  key={todo.id + todo.title}
+                ></TodoEdit>
+              ) : (
+                <Todo {...todo} key={todo.id + todo.title}></Todo>
+              )
+            )}
+        </div>
       </div>
     </div>
   )
