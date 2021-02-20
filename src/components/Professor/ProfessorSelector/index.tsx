c1684d91c725b4cbe544a334ef6251c90fe38f2c
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-02-20 23:46:30 +0900

    [#5] refactor : prettier 적용

--- src/components/Professor/ProfessorSelector/index.tsx
+++ src/components/Professor/ProfessorSelector/index.tsx
@@ -2,22 +2,28 @@ import React from 'react'
 import './style.scss'
 
 export interface SelectorBarProps {
-  selector:string[]
+  selector: string[]
   filterSelector: (value: string) => void
 }
 
-export const SelectorBar: React.FC<SelectorBarProps> = ({ selector, filterSelector }) => {
+export const SelectorBar: React.FC<SelectorBarProps> = ({
+  selector,
+  filterSelector,
+}) => {
   const onChangeSelect = (e) => {
-    filterSelector(e.target.value);
-  };
+    filterSelector(e.target.value)
+  }
 
   return (
     <div className="custom-select">
       <select onChange={onChangeSelect}>
         {selector.map((name, index) => (
-          <option key={index} value={name}>{name}</option>
+          <option key={index} value={name}>
+            {name}
+          </option>
         ))}
       </select>
+      <i className="f7-icons select-arrow ">chevron_down</i>
     </div>
-  );
-};
+  )
+}
