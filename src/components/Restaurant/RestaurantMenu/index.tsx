6e737a824e57e1ad4481cf91f052a3a338d47eb8
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-02-22 02:00:22 +0900

    [#7] feat : restaurant menu

---
+++ src/components/Restaurant/RestaurantMenu/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import './style.scss'
+
+export const RestaurantMenu: React.FC<{}> = () => {
+  return (
+    <div className="menus">
+      <ul className="menus-list">
+        <li className="menu">
+          <div>
+            <span className="name">김치볶음밥</span>
+          </div>
+          <div>
+            <span className="price">4,500원</span>
+          </div>
+        </li>
+        <li className="menu">
+          <div>
+            <span className="name">김치볶음밥</span>
+          </div>
+          <div>
+            <span className="price">4,500원</span>
+          </div>
+        </li>
+        <li className="menu">
+          <div>
+            <span className="name">김치볶음밥</span>
+          </div>
+          <div>
+            <span className="price">4,500원</span>
+          </div>
+        </li>
+        <li className="menu">
+          <div>
+            <span className="name">김치볶음밥</span>
+          </div>
+          <div>
+            <span className="price">4,500원</span>
+          </div>
+        </li>
+        <li className="menu">
+          <div>
+            <span className="name">김치볶음밥</span>
+          </div>
+          <div>
+            <span className="price">4,500원</span>
+          </div>
+        </li>
+        <li className="menu">
+          <div>
+            <span className="name">김치볶음밥</span>
+          </div>
+          <div>
+            <span className="price">4,500원</span>
+          </div>
+        </li>
+        <li className="menu">
+          <div>
+            <span className="name">김치볶음밥</span>
+          </div>
+          <div>
+            <span className="price">4,500원</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
