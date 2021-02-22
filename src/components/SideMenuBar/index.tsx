c696a376ed59b426498d5544c741aa36821a8656
choibumsu <bumsu0211@gmail.com>
2021-02-23 01:55:05 +0900

    [#20] refact : SideMenuBar MouseEnter, Leave 이벤트로 트랜지션 변경

--- src/components/SideMenuBar/index.tsx
+++ src/components/SideMenuBar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useStore } from '@/store'
 import { Bookmark } from '@/components/common/Bookmark'
 import './style.scss'
@@ -7,9 +7,20 @@ export const SideMenuBar: React.FC = () => {
   const {
     store: { sideBookmarkList },
   } = useStore()
+  const [isShow, setIsShow] = useState(false)
 
   return (
-    <div className="side-menu-bar">
+    <div
+      className={'side-menu-bar ' + (isShow ? 'show' : '')}
+      data-component=""
+      onMouseEnter={() => {
+        setIsShow(true)
+      }}
+      onMouseLeave={() => {
+        setIsShow(false)
+        document.body.click()
+      }}
+    >
       <div className="bookmark-container">
         {sideBookmarkList.map((bookmark) => (
           <Bookmark {...bookmark} key={bookmark.id}></Bookmark>
