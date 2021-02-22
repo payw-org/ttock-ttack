b6aca3bb3fc67e093e1492901bc256710a545524
choibumsu <bumsu0211@gmail.com>
2021-02-23 01:53:44 +0900

    [#20] feat : Bookmark 컴포넌트 메뉴 추가

--- src/components/common/Bookmark/index.tsx
+++ src/components/common/Bookmark/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useCallback, useRef, useState } from 'react'
+import { setElementAtCursor } from '@/utils/events'
 import './style.scss'
 
 export type BookmarkProps = {
@@ -9,12 +10,33 @@ export type BookmarkProps = {
 }
 
 export const Bookmark: React.FC<BookmarkProps> = ({ name, url, image }) => {
+  const menuContianer = useRef<HTMLDivElement>(null)
+  const [isShowMenu, setIsShowMenu] = useState(false)
+
+  const toggleMenu = (e) => {
+    e.preventDefault()
+    if (!menuContianer.current) return
+
+    setIsShowMenu(true)
+    setElementAtCursor(menuContianer.current, e)
+  }
+
   return (
-    <div className="bookmark">
+    <div className="bookmark" data-component={id}>
       <a href={url} target="_blank">
-        <img loading="lazy" src={image} alt={name} />
+        <img loading="lazy" src={image} alt={name} onContextMenu={toggleMenu} />
         <div className="name">{name}</div>
       </a>
+      <div
+        className={'menu-container ' + (isShowMenu ? 'show' : 'hide')}
+        ref={menuContianer}
+        onContextMenu={(e) => {
+          e.preventDefault()
+        }}
+      >
+        <div className="menu">수정</div>
+        <div className="menu">삭제</div>
+      </div>
     </div>
   )
 }
